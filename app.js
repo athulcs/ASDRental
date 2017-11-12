4215a9abaa6fea8173c052566444296290a5125f
arunjayaprakash <arun77733@gmail.com>
2017-11-12 17:15:52 +0530

    added express.static templates
    added express.static templates for loading css

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var mysql = require('mysql');
+var path = require('path');
 var connection = mysql.createConnection({
 
   host     : 'localhost',
@@ -15,7 +16,10 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.listen(3000,function(){
     console.log('Node server running @ http://localhost:3000')
 });
-
+app.use('/node_modules',  express.static(__dirname + '/node_modules'));
+//app.use('/templates/css',  express.static(__dirname + '/templates/css'));
+app.use(express.static('templates'))
+app.use(express.static(path.join(__dirname, 'templates/css')));
 app.get('/',function(req,res){
     res.sendFile('index.html',{'root': __dirname + '/templates'});
 
