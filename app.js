a3f0e36b81909f683169a019ea9598e59a09c5c8
arunjayaprakash <arun77733@gmail.com>
2017-11-12 14:12:45 +0530

    meet team hehe

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
-  database : 'mydb'
+  database : 'ASDdb'
 });
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // for parsing application/json
