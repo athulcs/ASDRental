2ed25e254a5ba7aa4d0a90b593886a59c4ae917f
arunjayaprakash <arun77733@gmail.com>
2017-11-12 10:53:50 +0530

    skeletal signin added

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'mydb'
+});
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
