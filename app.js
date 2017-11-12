72fc383c13b3fa9c052a54b29185176407de6723
AthulCS <athulcs.cs@gmail.com>
2017-11-12 16:18:10 +0530

    node / req added

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ var connection = mysql.createConnection({
 
   host     : 'localhost',
   user     : 'root',
-  password : '',
-  database : 'ASDdb'
+  password : 'root',
+  database : 'mydb'
 });
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.listen(3000,function(){
+    console.log('Node server running @ http://localhost:3000')
+});
+
+app.get('/',function(req,res){
+    res.sendFile('index.html',{'root': __dirname + '/templates'});
+
+});
