ae965c56e54eb6fe7dafed2a97013e4753a5a61c
arunjayaprakash <arun77733@gmail.com>
2017-11-18 11:00:46 +0530

    rent goes to rentinput page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
 
   host     : 'localhost',
   user     : 'root',
-  password : 'root',      //Change According to your mysql settings
+  password : '',      //Change According to your mysql settings
   database : 'carrent'
 });
 var bodyParser = require('body-parser');
@@ -41,7 +41,9 @@ app.get('/signin',function(req,res){
     res.sendFile('signin.html',{'root': __dirname + '/templates'});
 });
 
-
+app.get('/rentInput',function(req,res){
+    res.sendFile('rentinput.html',{'root': __dirname + '/templates'});
+});
 app.get('/lendPage',function(req,res){
     res.sendFile('lend.html',{'root': __dirname + '/templates'});
 });
